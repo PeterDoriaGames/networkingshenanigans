63b52e3a14035afff47d54f3ddf61dbda01fb424
Peter Doria <pdoriadev@gmail.com>
2019-09-02 17:12:42 -0700

    - Imported TextMeshPro - Created Launcher.unity scene for launching the game and creating a room. - MyLauncher script: Functionality to create/join rooms on start-up. Will modify so player decides when to join.

---
+++ NetworkingShenanigansUnityProject/Assets/_Scripts/MyLauncher.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MyLauncher : MonoBehaviourPunCallbacks
+{
+    #region PRIVATE SERIALIZABLE FIELDS
+
+    [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created.")]
+    [SerializeField]
+    private byte maxPlayersPerRoom = 4;
+
+
+
+    #endregion
+
+    #region PRIVATE FIELDS
+    /// <summary>
+    /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
+    /// </summary>
+    string gameVersion = "1";
+
+    #endregion
+
+    #region MONOBEHAVIOUR CALLBACKS
+
+    /// <summary>
+    /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+    /// </summary>
+    private void Awake()
+    {
+        // #Critical
+        // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically. 
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    /// <summary>
+    /// Monobehaviour method called on GameObject by Unity during initialization phase.
+    /// </summary>
+    private void Start()
+    {
+        Connect();
+    }
+
+    #endregion
+
+    #region PUBLIC METHODS
+
+    /// <summary>
+    /// Start the connection process. 
+    /// - If already connected, we attempt joining a random room  #RANDOM ROOM MAY NOT FIT GAME
+    /// - If not yet connected, Connect this application instance to Photon Cloud Network
+    /// </summary>
+    public void Connect()
+    {
+        
+        if (PhotonNetwork.IsConnected)
+        {
+            // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one. 
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            // #Critical, we must first and foremost connect to Phton Online Server
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    #endregion
+
+    #region MONOBEHAVIOUR PUN CALLBACKS 
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+        // #Critical: the first we try to do is to join a potential existing room. If there is, good, else, we'll be called back with OnJoinRandomFailed()
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room!");
+    }
+
+    #endregion
+}
